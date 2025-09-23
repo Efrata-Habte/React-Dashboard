4452528c9d970b890a6f0fd0312c2c9b0824bf8f
Efrata Habte <efratahabte1213@gmail.com>
2025-09-23 15:02:31 +0300

    Add card component styles to card.css for improved UI design

--- React-Dashboard/src/App.jsx
+++ React-Dashboard/src/App.jsx
@@ -1,12 +1,16 @@
 import Header from './Header';
 import Footer from './Footer';
+import Card from './Card';
+
 
 function App() {
 
   return (
     <>  
-      <Header />
-      <Footer />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </>
   );
 }
